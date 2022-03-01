9cd879ff61c9b8df00dafc6a419197807196bfc5
arsenblade <91114536+arsenblade@users.noreply.github.com>
2022-03-01 19:55:36 +0500

    Финальное добавление модулей

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createTripFilter} from './view/filter-view.js';
 import {createTripContent} from './view/list-view.js';
 import {createTripSort} from './view/sort-view.js';
 import {createTripAddNewPoint} from './view/add-new-point-view.js';
+import {createTripEditPoint} from './view/edit-point-view.js';
 import {renderTemplate, RenderPosition} from './render.js';
 
 const siteHeader = document.querySelector('.page-header');
@@ -23,5 +24,7 @@ renderTemplate(siteMainElement, createTripContent(), RenderPosition.BEFOREEND);
 
 const siteList = document.querySelector('.trip-events__list');
 
-renderTemplate(siteList, createTripAddNewPoint(), RenderPosition.BEFOREEND);
+renderTemplate(siteList, createTripAddNewPoint(), RenderPosition.AFTERBEGIN);
+
+renderTemplate(siteList, createTripEditPoint(), RenderPosition.AFTERBEGIN);
 
